3b09f2f3233cbe3dcca0d7b2a211d52dab9dadfa
Mizuki <m.minegishi.sys23@morijyobi.ac.jp>
2023-11-09 14:59:24 +0900

    BMI二人分の計算

--- Main.java
+++ Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double n = sc.nextDouble();
-        double a = sc.nextDouble();
-        double b = a / (n * n);
-        System.out.println(String.format ("%.2f", b));
+        for (int i = 0; i < 2; i++) {
 
+            Scanner sc = new Scanner(System.in);
+            System.out.println("身長を入力してください");
+            double n = sc.nextDouble();
+            System.out.println("体重を入力してください");
+            double a = sc.nextDouble();
+            double b = a / (n * n);
+            System.out.println("あなたのBMIは" + String.format("%.2f", b));
+        }
     }
 }
